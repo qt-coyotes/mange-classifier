c732843f36c9a9713b44cf17e9508ca2147d8c38
Steven Tang <18170455+steventango@users.noreply.github.com>
2023-03-20 23:58:10 -0600

    cfn=10

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ def main():
     criterions = {
         "BCEWithLogitsLoss": nn.BCEWithLogitsLoss(),
         "MacroSoftFBetaLoss": BinaryMacroSoftFBetaLoss(2),
-        "ExpectedCostLoss": BinaryExpectedCostLoss(cfn=5),
+        "ExpectedCostLoss": BinaryExpectedCostLoss(cfn=10),
     }
     parser = argparse.ArgumentParser()
     parser = Trainer.add_argparse_args(parser)
