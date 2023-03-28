d0414d31cf031a9ea6a2eac054aeffe150725bcb
Steven Tang <18170455+steventango@users.noreply.github.com>
2023-03-28 10:32:28 -0600

    cfn=5

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ def main():
     criterions = {
         "BCELoss": nn.BCELoss(),
         "MacroSoftFBetaLoss": BinaryMacroSoftFBetaLoss(2),
-        "ExpectedCostLoss": BinaryExpectedCostLoss(cfn=50),
+        "ExpectedCostLoss": BinaryExpectedCostLoss(cfn=5),
         "SurrogateFBetaLoss": BinarySurrogateFBetaLoss(2)
     }
     parser = argparse.ArgumentParser()
