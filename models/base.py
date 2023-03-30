b7dce6f186d1e979e6ba36f88b0707e4c3cbb2f7
Steven Tang <18170455+steventango@users.noreply.github.com>
2023-03-29 19:08:22 -0600

    Fix gaussian blur

--- models/base.py
+++ models/base.py
@@ -30,7 +30,7 @@ class BaseModel(LightningModule):
             transforms.RandomHorizontalFlip(),
             transforms.RandomRotation(10),
             transforms.RandomPerspective(),
-            transforms.GaussianBlur(10),
+            transforms.GaussianBlur(3, simga=(0, 2)),
             transforms.ColorJitter(
                 brightness=0.1, contrast=0.1, saturation=0.1, hue=0.1
             ),
