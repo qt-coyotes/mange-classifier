21a327b82a405bed361b3cf053cfb8d125443fc2
Steven Tang <18170455+steventango@users.noreply.github.com>
2023-04-03 15:37:13 -0600

    More extreme data augmentation

--- models/base.py
+++ models/base.py
@@ -40,11 +40,11 @@ class BaseModel(LightningModule):
         )
         self.augmentations = transforms.Compose([
             transforms.RandomHorizontalFlip(),
-            transforms.RandomRotation(10),
+            transforms.RandomRotation(15),
             # transforms.RandomPerspective(),
             transforms.GaussianBlur(3, sigma=(0.1, 2)),
             transforms.ColorJitter(
-                brightness=0.1, contrast=0.1, saturation=0.1, hue=0.1
+                brightness=0.2, contrast=0.2, saturation=0.2, hue=0.2
             ),
         ])
         self.criterion = criterion
