e3850b772ebe3f93bc0fe24ccfde750cbb61539f
Tim van Maren <vanmaren@ualberta.ca>
2023-04-04 01:11:35 -0600

    KO Month Only

--- data.py
+++ data.py
@@ -58,10 +58,10 @@ class COCOImageDataset(Dataset):
             tabular = torch.tensor(
                 [
                     image["is_color"],
-                    # (image["year"] - self.tabular_transform["mean"]["year"])
-                    # / self.tabular_transform["std"]["year"],
-                    (image["month"] - self.tabular_transform["mean"]["month"])
-                    / self.tabular_transform["std"]["month"],
+                    (image["year"] - self.tabular_transform["mean"]["year"])
+                    / self.tabular_transform["std"]["year"],
+                    # (image["month"] - self.tabular_transform["mean"]["month"])
+                    # / self.tabular_transform["std"]["month"],
                     # image["day"],
                     (image["hour"] - self.tabular_transform["mean"]["hour"])
                     / self.tabular_transform["std"]["hour"],
