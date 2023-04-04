1270eb16d45b4434f9e35029dc552832e5aae511
Steven Tang <18170455+steventango@users.noreply.github.com>
2023-04-03 21:28:34 -0600

    Drop year

--- data.py
+++ data.py
@@ -58,8 +58,8 @@ class COCOImageDataset(Dataset):
             tabular = torch.tensor(
                 [
                     image["is_color"],
-                    (image["year"] - self.tabular_transform["mean"]["year"])
-                    / self.tabular_transform["std"]["year"],
+                    # (image["year"] - self.tabular_transform["mean"]["year"])
+                    # / self.tabular_transform["std"]["year"],
                     (image["month"] - self.tabular_transform["mean"]["month"])
                     / self.tabular_transform["std"]["month"],
                     # image["day"],
