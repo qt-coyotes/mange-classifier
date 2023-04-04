b5c9522bfe119a717b286a6911602a091eea7dc2
Tim van Maren <vanmaren@ualberta.ca>
2023-04-03 23:48:15 -0600

    Knockout Lat Long

--- data.py
+++ data.py
@@ -63,19 +63,19 @@ class COCOImageDataset(Dataset):
                     (image["month"] - self.tabular_transform["mean"]["month"])
                     / self.tabular_transform["std"]["month"],
                     # image["day"],
-                    # (image["hour"] - self.tabular_transform["mean"]["hour"])
-                    # / self.tabular_transform["std"]["hour"],
+                    (image["hour"] - self.tabular_transform["mean"]["hour"])
+                    / self.tabular_transform["std"]["hour"],
                     # image["minute"],
-                    (
-                        image["latitude"]
-                        - self.tabular_transform["mean"]["latitude"]
-                    )
-                    / self.tabular_transform["std"]["latitude"],
-                    (
-                        image["longitude"]
-                        - self.tabular_transform["mean"]["longitude"]
-                    )
-                    / self.tabular_transform["std"]["longitude"],
+                    # (
+                    #     image["latitude"]
+                    #     - self.tabular_transform["mean"]["latitude"]
+                    # )
+                    # / self.tabular_transform["std"]["latitude"],
+                    # (
+                    #     image["longitude"]
+                    #     - self.tabular_transform["mean"]["longitude"]
+                    # )
+                    # / self.tabular_transform["std"]["longitude"],
                 ],
                 dtype=torch.float32,
             )
