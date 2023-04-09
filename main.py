87161a13f752863c32b711d89fca3fe885acd40c
Steven Tang <18170455+steventango@users.noreply.github.com>
2023-04-09 16:20:55 -0600

    check if large models fit

--- main.py
+++ main.py
@@ -43,39 +43,39 @@ NO_TRAIN_MODELS = {
 }
 
 SUPER_LEARNER_MODELS = {
-    "ResNet18": (ResNetModel, 18),
-    "ResNet34": (ResNetModel, 34),
-    "ResNet50": (ResNetModel, 50),
-    "ResNet101": (ResNetModel, 101),
-    "ResNet152": (ResNetModel, 152),
-    "ViT-B/16": (ViTModel, "B/16"),
-    "ViT-B/32": (ViTModel, "B/32"),
-    "ViT-L/16": (ViTModel, "L/16"),
-    "ViT-L/32": (ViTModel, "L/32"),
+    # "ResNet18": (ResNetModel, 18),
+    # "ResNet34": (ResNetModel, 34),
+    # "ResNet50": (ResNetModel, 50),
+    # "ResNet101": (ResNetModel, 101),
+    # "ResNet152": (ResNetModel, 152),
+    # "ViT-B/16": (ViTModel, "B/16"),
+    # "ViT-B/32": (ViTModel, "B/32"),
+    # "ViT-L/16": (ViTModel, "L/16"),
+    # "ViT-L/32": (ViTModel, "L/32"),
     "ViT-H/14": (ViTModel, "H/14"),
-    "DenseNet121": (DenseNetModel, 121),
-    "DenseNet161": (DenseNetModel, 161),
-    "DenseNet169": (DenseNetModel, 169),
+    # "DenseNet121": (DenseNetModel, 121),
+    # "DenseNet161": (DenseNetModel, 161),
+    # "DenseNet169": (DenseNetModel, 169),
     "DenseNet201": (DenseNetModel, 201),
-    "yolov8n-cls.pt": (YoloModel, "yolov8n-cls.pt"),
-    "yolov8s-cls.pt": (YoloModel, "yolov8s-cls.pt"),
-    "yolov8m-cls.pt": (YoloModel, "yolov8m-cls.pt"),
-    "yolov8l-cls.pt": (YoloModel, "yolov8l-cls.pt"),
+    # "yolov8n-cls.pt": (YoloModel, "yolov8n-cls.pt"),
+    # "yolov8s-cls.pt": (YoloModel, "yolov8s-cls.pt"),
+    # "yolov8m-cls.pt": (YoloModel, "yolov8m-cls.pt"),
+    # "yolov8l-cls.pt": (YoloModel, "yolov8l-cls.pt"),
     "yolov8x-cls.pt": (YoloModel, "yolov8x-cls.pt"),
 }
 
 MODELS = {**NO_TRAIN_MODELS, **SUPER_LEARNER_MODELS}
 
 LEARNING_RATES = {
-    0.001,
-    0.0001,
-    0.00001,
+    # 0.001,
+    # 0.0001,
+    # 0.00001,
     -1
 }
 
 BATCH_SIZES = {
-    16,
-    32,
+    # 16,
+    # 32,
     64
 }
 
@@ -362,6 +362,7 @@ def internal_cross_validation(datamodule: LightningDataModule):
         ("--no_data_augmentation", ""),
         ("--no_tabular_features", ""),
     )
+    print(len(list(argvs)))
     for argv in argvs:
         argv = list(filter(len, argv))
         args = parse_args(argv)
