c84c27b3641ecaa2776c47a20d0152ea2a246363
Steven Tang <18170455+steventango@users.noreply.github.com>
2023-04-18 21:39:36 -0600

    Try this

--- main.py
+++ main.py
@@ -537,7 +537,8 @@ def captum_identify(args: argparse.Namespace):
         noise_tunnel = NoiseTunnel(integrated_gradients)
         input = transform(image)
         input = input.unsqueeze(0)
-        output = model.forward((input, torch.tensor([])))
+        input = (input, torch.tensor([]))
+        output = model.forward(input)
         output = F.softmax(output, dim=0)
         label = torch.tensor(
             [[1 if torch.sigmoid(output) > 0.5 else 0]]
